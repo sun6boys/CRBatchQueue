3d84a5bff82552a52535b416f9fc31f1ba0c7171
user <sun6boys@outlook.com>
2017-04-19 13:26:40 +0800

    Initial Commit

---
+++ CRBatchQueue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CRBatchQueue
+//
+//  Created by user on 17/4/19.
+//  Copyright © 2017年 Charon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
